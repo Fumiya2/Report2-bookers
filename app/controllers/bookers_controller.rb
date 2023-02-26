8a9e0a726c476aed5f5ef649a31100d5ec6f2a9c
Fumiya2 <124821309+Fumiya2@users.noreply.github.com>
2023-02-26 12:01:51 +0000

    [Update]

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -1,7 +1,15 @@
 class BookersController < ApplicationController
+  
   def new
+    @book = Book.new
   end
 
+  def create
+    book =Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+  
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookers/new'
+  get 'bookers' => 'books#new'
   get 'bookers/index'
   get 'bookers/show'
   get 'bookers/edit'
