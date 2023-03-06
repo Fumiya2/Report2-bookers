b856216cfc192a51609bf4804c99983be8672ae7
Fumiya2 <124821309+Fumiya2@users.noreply.github.com>
2023-03-06 09:39:54 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
+    book.save 
     redirect_to '/books'
   end
 
@@ -20,8 +20,15 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @books =Book.find()
+    @books =Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
   
   def destroy
   end
